45c7eed0c1cee3e0f41c177f003b942d1de5d9c9
Simon Miner <simon.miner@gmail.com>
2022-05-14 14:07:15 -0400

    Refactor background.js code

--- background.js
+++ background.js
@@ -1,44 +1,57 @@
-// Inspired by https://stackoverflow.com/questions/16136275.
+/** Turn the extensino on and off when its icon is pressed.
+ * Inspired by https://stackoverflow.com/questions/16136275.
+ */
 var isExtensionOn = false;
-
-function turnOffScreenReader () {
-    document._screenReader.cleanUp();
-}
-
 chrome.action.onClicked.addListener((tab) => {
     isExtensionOn = !isExtensionOn;
 
     if (isExtensionOn) {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ['site-unseen.min.js']
-        });
-        chrome.action.setIcon({
-            path: "icon16-on.png",
-            tabId:tab.id
-        });
+        turnOnExtension(tab);
     }
     else {
-        chrome.action.setIcon({
-            path: "icon16.png",
-            tabId:tab.id
-        });
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: turnOffScreenReader
-        });
+        turnOffExtension(tab);
     }
 });
 
+/** Keep the extension turned on when the page is (re)loaded).
+ * Inspired by https://stackoverflow.com/questions/6497548.
+ */
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete' && tab.active && isExtensionOn) {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ['site-unseen.min.js']
-        });
-        chrome.action.setIcon({
-            path: "icon16-on.png",
-            tabId:tab.id
-        });
+        turnOnExtension(tab);
     }
 })
+
+/**
+ * Activate the extension and change its
+ * icon to show it is enabled.
+ */
+function turnOnExtension(tab) {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['site-unseen.min.js']
+    });
+    chrome.action.setIcon({
+        path: "icon16-on.png",
+        tabId:tab.id
+    });
+}
+
+/**
+ * Deactivate the extension and change its
+ * icon to show it is disabled.
+ */
+function cleanUpScreenReader () {
+    document._screenReader.cleanUp();
+}
+
+function turnOffExtension(tab) {
+    chrome.action.setIcon({
+        path: "icon16.png",
+        tabId:tab.id
+    });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: cleanUpScreenReader
+    });
+}
